937f4b74c82d45383f489fa6d7984348be476e9e
Saikat Ghosh <ghosh_saikat4000@yahoo.co.in>
2017-05-21 21:27:21 +0530

    Add files via upload

---
+++ C Programs/C Programs - 1/Introduction_to_Nim.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+    int no_of_test_cases, i, no_of_piles, no_of_stones_in_current_pile, bitwise_XOR_all_piles;
+
+    scanf("%d",&no_of_test_cases);
+
+    while(no_of_test_cases-- != 0)
+    {
+        scanf("%d", &no_of_piles);
+
+        bitwise_XOR_all_piles = 0;
+        for(i = 1; i <= no_of_piles; i++)
+        {
+            scanf("%d",&no_of_stones_in_current_pile);
+            bitwise_XOR_all_piles = bitwise_XOR_all_piles^no_of_stones_in_current_pile;
+        }
+
+        printf(bitwise_XOR_all_piles == 0 ? "Second\n" : "First\n"); //The winning strategy is to give your opponent a configuration where the piles XOR to 0
+    }
+}
