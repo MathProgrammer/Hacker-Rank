ceb54b06db56dd7af974a6b0199fa04f2e1c0209
Saikat Ghosh <ghosh_saikat4000@yahoo.co.in>
2017-06-04 11:28:24 +0530

    Update Learning_From_The_Past.c

--- C Programs/C Programs - 3/Learning_From_The_Past.c
+++ C Programs/C Programs - 3/Learning_From_The_Past.c
@@ -1,1 +1,22 @@
+#include <stdio.h>
 
+#define min(a, b) (a < b ? a : b)
+#define max(a, b) (a > b ? a : b)
+
+int main()
+{
+    int no_of_days, x, y, z, i, minimum, max_profit_day, max_overall_profit = 0;
+    scanf("%d", &no_of_days);
+
+    for(i = 1 ; i <= no_of_days; i++)
+    {
+        scanf("%d %d %d", &x, &y, &z);
+        minimum = min(x, min(y, z));
+        max_profit_day = (x + y + z) - minimum;
+
+        max_overall_profit = max(max_overall_profit, max_profit_day);
+    }
+
+    printf("%d\n",max_overall_profit);
+    return 0;
+}
