38238daabb1d793b2922f1a46d6004a3625721ed
Saikat Ghosh <ghosh_saikat4000@yahoo.co.in>
2018-04-10 23:27:44 +0530

    Update Transform to Palindrome.cpp

--- C Programs/C Programs - 7/Transform to Palindrome.cpp
+++ C Programs/C Programs - 7/Transform to Palindrome.cpp
@@ -1,64 +1,80 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
 
-typedef long long LL;
+#define max(a, b) (a > b ? a : b)
 using namespace std;
 
-void solve()
+const int MAX_LETTERS = 1e5 + 5;
+vector <int> transform[MAX_LETTERS];
+vector <int> component(MAX_LETTERS, 0);
+
+void dfs_and_mark(int v, int component_no)
 {
-    int no_of_elements, no_of_segments;
-    cin >> no_of_elements >> no_of_segments;
+    component[v] = component_no;
 
-    vector <LL> A(no_of_elements + 1);
-    for(int i = 1; i <= no_of_elements; i++)
-        cin >> A[i];
-
-    vector <int> ending(no_of_segments + 1);
-    ending[0] = 0;
-    for(int i = 1; i <= no_of_segments; i++)
-        cin >> ending[i];
-
-    const LL oo = 1e16;
-    vector <LL> max_starting_from(no_of_elements + 1);
-    vector <LL> best_add_option(no_of_segments + 2, -oo);
-    vector <LL> best_subtract_option(no_of_segments + 2, -oo);
-
-    best_add_option[no_of_segments + 1] = best_subtract_option[no_of_segments + 1] = 0;
-
-    for(int i = ending[no_of_segments]; i > ending[no_of_segments - 1]; i--)
+    for(int i = 0; i < transform[v].size(); i++)
     {
-        max_starting_from[i] = A[i];
-        best_add_option[no_of_segments] = max(best_add_option[no_of_segments], A[i]);
-        best_subtract_option[no_of_segments] = max(best_subtract_option[no_of_segments], -A[i]);
+        int child = transform[v][i];
+
+        if(component[child] == 0)
+            dfs_and_mark(child, component_no);
     }
+}
 
-    for(int segment = no_of_segments - 1; segment > 0; segment--)
+int find_longest_palindromic_subsequence(vector <int> &input, int length)
+{
+    typedef vector <int> v_int;
+    vector <v_int> longest_palindrome(length + 1, v_int(length + 1, 0));
+
+    for(int i = 1; i <= length; i++)
+        longest_palindrome[i][i] = 1;
+
+    for(int current_length = 2; current_length <= length; current_length++)
     {
-        for(int i = ending[segment]; i > ending[segment - 1]; i--)
+        for(int start = 1, end = start + current_length - 1; end <= length; start++, end++)
         {
-            max_starting_from[i] = max(A[i] + best_subtract_option[segment + 1], -A[i] + best_add_option[segment + 1]);
+            longest_palindrome[start][end] = max(longest_palindrome[start + 1][end], longest_palindrome[start][end - 1]);
 
-            best_add_option[segment] = max(best_add_option[segment], A[i] + max_starting_from[i]);
-            best_subtract_option[segment] = max(best_subtract_option[segment], -A[i] + max_starting_from[i]);
+            if(component[input[start]] == component[input[end]])
+            {
+                longest_palindrome[start][end] = max(longest_palindrome[start][end], 2 + longest_palindrome[start + 1][end - 1]);
+            }
         }
     }
 
-    LL answer = -oo;
-
-    for(int i = 1; i <= ending[1]; i++)
-        answer = max(answer, max_starting_from[i]);
-
-    cout << answer << "\n";
+    return longest_palindrome[1][length];
 }
 
 int main()
 {
-    int no_of_test_cases;
-    cin >> no_of_test_cases;
+    int no_of_letters, no_of_transformations, length;
+    cin >> no_of_letters >> no_of_transformations >> length;
 
-    while(no_of_test_cases--)
-        solve();
+    for(int i = 1; i <= no_of_transformations; i++)
+    {
+        int x, y;
+        cin >> x >> y;
+
+        transform[x].push_back(y);
+        transform[y].push_back(x);
+    }
+
+    vector <int> input(length + 1);
+    for(int i = 1; i <= length; i++)
+        cin >> input[i];
+
+    int component_no = 1;
+    for(int i = 1; i <= no_of_letters; i++)
+    {
+        if(component[i] == 0)
+        {
+            dfs_and_mark(i, component_no++);
+        }
+    }
+
+    int longest_palindromic_subsequence = find_longest_palindromic_subsequence(input, length);
+
+    cout << longest_palindromic_subsequence;
 
     return 0;
 }
