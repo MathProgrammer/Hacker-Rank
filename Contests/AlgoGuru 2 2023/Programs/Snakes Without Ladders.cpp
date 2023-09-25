cbdf17e8371ef81024630d5fb2f6ff8f625e64b7
Saikat Ghosh <ghosh_saikat4000@yahoo.co.in>
2023-09-25 10:35:24 +0530

    Create Snakes Without Ladders.cpp

---
+++ Contests/AlgoGuru 2 2023/Programs/Snakes Without Ladders.cpp
@@ -0,0 +1,48 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+long long ceil(long long numerator, long long denominator)
+{
+    return numerator/denominator + (numerator%denominator != 0);
+}
+
+void solve()
+{
+    int moves;
+    long long snakes;
+    cin >> snakes >> moves; 
+    
+    if(moves == 1)
+    {
+        cout << (snakes == 0 ? 1 : -1) << "\n";
+        return ;
+    }
+    
+    long long empty_squares_needed = 1 + ceil(snakes, moves - 1);
+    long long squares = snakes + empty_squares_needed; 
+    long long rows = sqrt(2*squares);
+    while(rows*(rows + 1) < 2*squares)
+    {
+        rows++;
+    }
+ 
+    cout << rows << "\n";
+}
+
+int main() 
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int no_of_test_cases; 
+    cin >> no_of_test_cases; 
+    
+    while(no_of_test_cases--)
+        solve();
+    
+    return 0;
+}
